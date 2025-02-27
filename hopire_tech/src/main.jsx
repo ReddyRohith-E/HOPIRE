9b789e7a0cb1a369cfe3e0eea47a8d70327adf02
Rohith Enduluri <e.reddyrohith2004@gmail.com>
2025-02-27 23:24:38 +0530

    Portfolio section added

--- hopire_tech/src/main.jsx
+++ hopire_tech/src/main.jsx
@@ -5,11 +5,13 @@ import Navbar from './assets/Components/Navbar/Navbar.jsx';
 import './index.css';
 import Footer from './assets/Components/Footer/Footer.jsx';
 import ClientTestimonials from './assets/Components/Testimonials/ClientTestimonials.jsx';
+import Portfolio from './assets/Components/Portfolio/Portfolio.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Navbar />
+    <Portfolio/>
     <ClientTestimonials/>
       <Footer/>
     </BrowserRouter>
