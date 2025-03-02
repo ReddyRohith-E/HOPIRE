a3832914750a60f690c835ef1b86531aa24bbfac
Rohith Enduluri <e.reddyrohith2004@gmail.com>
2025-03-02 12:35:19 +0530

    Fixed base URL in Vite configuration

--- hopire_tech/vite.config.js
+++ hopire_tech/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'HOPIRE/hopire_tech/', // Set this to match your GitHub Pages URL path
+  base: '/HOPIRE/hopire_tech/', // Set this to match your GitHub Pages URL path
   plugins: [react()]
 });
