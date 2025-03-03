389d899b81b198b57be9830f1ca79430de010729
Rohith Enduluri <e.reddyrohith2004@gmail.com>
2025-03-03 21:47:34 +0530

    Get started moved

--- hopire_tech/src/assets/Components/Hero/Hero.jsx
+++ hopire_tech/src/assets/Components/Hero/Hero.jsx
@@ -1,22 +1,32 @@
 import "./hero.css";
-import { Element } from "react-scroll"; 
+import { Element, scroller } from "react-scroll";
 import heroBackground from "/hero-background.jpeg";
 
 const Hero = () => {
+
+    const handleGetStartedClick = () => {
+        scroller.scrollTo('contact', {
+            duration: 300,
+            delay: 0,
+            offset: -80,
+            smooth: 'easeInOutQuart'
+        });
+    };
+
     return (
         <Element name="home">
-        <div className="hero" style={{ backgroundImage: `url(${heroBackground})` }}>
-            <h1>Hopire</h1>
-            <h2>Tech Solutions</h2>
-            <div className="disturbance"></div>
-            <div className="overlay"></div>
-            <div className="circle"></div>
-            <div className="triangle"></div>
-            <div className="stick"></div>
-            <button>Get Started</button>
-        </div>
+            <div className="hero" style={{ backgroundImage: `url(${heroBackground})` }}>
+                <h1>Hopire</h1>
+                <h2>Tech Solutions</h2>
+                <div className="disturbance"></div>
+                <div className="overlay"></div>
+                <div className="circle"></div>
+                <div className="triangle"></div>
+                <div className="stick"></div>
+                <button onClick={handleGetStartedClick}>Get Started</button>
+            </div>
         </Element>
     );
 };
 
-export default Hero; 
+export default Hero;
